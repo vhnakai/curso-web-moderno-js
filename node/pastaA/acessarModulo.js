b0fa3d7a91750ebb59949213f27e3c80ec3c0829
vhnakai <36956647+vhnakai@users.noreply.github.com>
2019-12-18 15:24:58 -0300

    modulo node

---
+++ node/pastaA/acessarModulo.js
@@ -0,0 +1,1 @@
+const moduloB = require('../moduloB')
--- node/usandoModulosTerc.js
+++ node/usandoModulosTerc.js
@@ -1,2 +1,2 @@
 const _= require('lodash')
-setInterval(() => console.log(_.random(1,1000)), 200)
+setInterval(() => console.log(_.random(1,1000)), 2000)
