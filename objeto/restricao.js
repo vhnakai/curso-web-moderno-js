957f4da1a1d6e628c29512ceb31d843891c78879
vhnakai <hugo.nakai@gmail.com>
2020-01-07 17:09:16 -0300

    restricoues de objetos

---
+++ objeto/restricao.js
@@ -0,0 +1,22 @@
+// object.preventExtensions
+const produto = Object.preventExtensions({
+    nome: 'Qualquer',
+    preco : 199, 
+    tag: 'promocao'
+})
+
+console.log('extensivel:', Object.isExtensible(produto))
+produto.descricao = 'Boracha'
+delete produto.tag
+console.log(produto)
+
+//object.seal
+
+const pessoa = { nome: 'Juliana', idade: 34}
+Object.seal(pessoa)
+delete pessoa.nome
+pessoa.nome = 'katia'
+console.log('Selado:', Object.isSealed(pessoa))
+console.log(pessoa)
+
+//object.freeze = selado  valor constante
