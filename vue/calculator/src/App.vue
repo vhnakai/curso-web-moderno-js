fa0eebcb7ce513f31f7a7237c731a63d224f95c1
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-02-03 10:06:23 -0300

    vue calculator and gitignore fix

---
+++ vue/calculator/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <h1>Olá Vue</h1>
+        <Calculator />
+    </div>
+</template>
+
+<script>
+
+import Calculator from "./main/Calculator"
+export default {
+    components : { Calculator}
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73))
+}
+</style>
