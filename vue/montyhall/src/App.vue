b4ac776d90e02fd8dae8e39fd97690bb2fa3204c
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-02-03 17:09:08 -0300

    montyhall

---
+++ vue/montyhall/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Problema monthyhall</h1>
+    <Door number="1" :hasGift="false" />
+  </div>
+</template>
+
+<script>
+
+import Door from './components/Door'
+export default {
+  name: 'App',
+  components:{ Door }
+
+}
+</script>
+
+<style>
+
+body{
+  color: #fff;
+  background: linear-gradient( to right,#314755,#26a0da) ;
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1{
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+
+}
+
+</style>
---
+++ vue/montyhall/src/components/Door.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="door-area">
+      <div class="door-frame">
+          <Gift v-if="open && hasGift" />
+          <div class="door">
+              <div class="number">
+                  {{number}}
+              </div>
+              <div class="knob"></div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import Gift from './Gift'
+export default {
+    name: 'Door',
+    components: { Gift },
+    props:{
+        number: {},
+        hasGift: {type: Boolean}
+    },
+    data: function() {
+        return{
+            open: false,
+            selected: false
+        }        
+    }
+}
+</script>
+
+<style>
+
+:root{
+    --door-border: 5px solid brown;
+    
+}
+
+.door-area{
+    position: relative;
+    width: 200px;
+    height: 300px;
+    border-bottom: 10px solid #AAA;
+    margin-bottom: 20px;
+    font-size: 3rem;
+    display: flex;
+    justify-content: center;
+
+}
+
+.door-frame{
+    position: absolute;
+    height: 300px;
+    width: 180px;
+
+    border-left: var(--door-border);
+    border-top: var(--door-border);
+    border-right: var(--door-border);
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.door{
+    position: absolute;
+    top: 25px;
+    height: 295px;
+    width: 170px;
+    background-color: chocolate;
+
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+}
+</style>
