934ca2c2da0a237740bda1a29f94dacab142bde7
vhnakai <36956647+vhnakai@users.noreply.github.com>
2020-02-04 13:55:45 -0300

    configuraçao de quantide de porta

--- vue/montyhall/src/App.vue
+++ vue/montyhall/src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
     <h1>Problema monthyhall</h1>
-    <Door number="1" :hasGift="true" />
+    <div class="form">
+      <div v-if="!started">
+        <label for="portsAmount">Quantas portas?</label>
+        <input type="text" id="portsAmount" size="3" 
+            v-model.number="portasAmount">
+      </div>
+      <div v-if="!started">
+        <label for="selectedPort">Qual porta é premiada? </label>
+        <input type="text" id="selectedPort" size="3" 
+            v-model.number="selectedPort">
+      </div>
+      <button v-if="!started" @click="started = true">Start</button>
+      <button  v-if="started" @click="started = false">Restart</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i ==selectedPort" :number="i" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,7 +28,14 @@
 import Door from './components/Door'
 export default {
   name: 'App',
-  components:{ Door }
+  components:{ Door },
+  data: function(){
+    return{
+      started: false,
+      portsAmount: 3,
+      selectedPort: null
+    }
+  }
 
 }
 </script>
